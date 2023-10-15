d49e3ee0571806a2c8a2fc6bcce7c54a22ee2c86
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-10-15 15:05:53 +0530

    Add files via upload

---
+++ CaseCheck.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if (c >= 'a' && c <= 'z') {
+            System.out.println("Lower case");
+        } else {
+            System.out.println("Upper Case");
+        }
+
+    }
+}
