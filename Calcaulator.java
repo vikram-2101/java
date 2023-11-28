f12fbae52d2735d5ea5e898adedcf74c7cf18c61
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-28 22:12:25 +0530

    Update Calcaulator.java

--- Calcaulator.java
+++ Calcaulator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Calcaulator { f
+public class Calcaulator { 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
        
