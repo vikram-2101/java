ae9238da646759e124a7f73a91d3ea0e75e03fa9
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-12-04 19:06:38 +0530

    Update stringBuilder.java

--- stringBuilder.java
+++ stringBuilder.java
@@ -1,4 +1,4 @@
-nbfstshimport java.util.*;
+lnbfstshimport java.util.*;
 public class stringBuilder {
     public static void main(String args[]) {
         StringBuilder sc = new StringBuilder("hello");
